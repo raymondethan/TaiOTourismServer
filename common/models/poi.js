5ced624957491256320e2ceb0d1c78f982e191b7
Ethan Raymond <ethan.raymond14@gmail.com>
2016-05-13 23:07:15 +0800

    cleaned up code

--- common/models/poi.js
+++ common/models/poi.js
@@ -1,13 +1,6 @@
 module.exports = function(Poi) {
     Poi.getUpdate = function(date, cb) {
         _d = new Date(date);
-        console.log(_d);
-        //console.log(date);
-        // Poi.findById( 1, function (err, instance) {
-        //     response = "Name of coffee shop is " + instance.name;
-        //     cb(null, response);
-        //     console.log(response);
-        // });
         Poi.find({where: {lastModified: {gt: _d}}},function (err, posts) {
             if (null != posts && posts.length > 0) {
                 response = "Name of coffee shop is " + posts[0].name;
@@ -17,7 +10,6 @@ module.exports = function(Poi) {
             }
             cb(null, posts);
             console.log(response);});
-        // cb(null,Poi.find({where: {id: 1}},function (err, posts) { console.log("error: " + err + ", " + posts)} ));
     }
     Poi.remoteMethod('getUpdate',{
         http: {path: '/getupdate', verb: 'get'},
