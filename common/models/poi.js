9a84283e36bea8e7a0c319ef091a27812651d242
Ethan Raymond <ethan.raymond14@gmail.com>
2016-05-13 23:06:04 +0800

    server sends back modified POIs

--- common/models/poi.js
+++ common/models/poi.js
@@ -1,3 +1,27 @@
 module.exports = function(Poi) {
-
+    Poi.getUpdate = function(date, cb) {
+        _d = new Date(date);
+        console.log(_d);
+        //console.log(date);
+        // Poi.findById( 1, function (err, instance) {
+        //     response = "Name of coffee shop is " + instance.name;
+        //     cb(null, response);
+        //     console.log(response);
+        // });
+        Poi.find({where: {lastModified: {gt: _d}}},function (err, posts) {
+            if (null != posts && posts.length > 0) {
+                response = "Name of coffee shop is " + posts[0].name;
+            }
+            else {
+                response = "no posts";
+            }
+            cb(null, posts);
+            console.log(response);});
+        // cb(null,Poi.find({where: {id: 1}},function (err, posts) { console.log("error: " + err + ", " + posts)} ));
+    }
+    Poi.remoteMethod('getUpdate',{
+        http: {path: '/getupdate', verb: 'get'},
+        accepts: {arg: 'date', type: 'string', http: { source: 'query' } },
+        returns: {arg: 'pois', type: 'string'}
+    });
 };
--- server/boot/create-sample-model.js
+++ server/boot/create-sample-model.js
@@ -1,6 +1,25 @@
-module.exports = function(app) {
-  app.dataSources.mysqlDs.automigrate('POI', function(err) {
-    if (err) throw err;
- 
-  });
-};
+// module.exports = function(app) {
+//   app.dataSources.mysqlDs.automigrate('POI', function(err) {
+//     if (err) throw err;
+//         app.models.POI.create([
+//           {
+//             "name": "poi1",
+//             "coordinates": {
+//               "lat": 22.253155,
+//               "lng": 113.858185
+//             },
+//             "category": "Tour_Stop",
+//             "tourOrder": 1,
+//             "description": "Description for tour stop 1",
+//             "openingHours": "08:00:00",
+//             "counter": 0,
+//             "lastModified": "2016-05-10T00:00:00.000Z",
+//             "id": 1
+//           }
+//         ], function(err, pois) {
+//           if (err) throw err;
+     
+//           console.log('Models created: \n', pois);
+//         });
+//   });
+// };
