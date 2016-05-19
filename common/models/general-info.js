26d36502c6dd810cb2ecd5a1c9da0669a222b937
Ethan Raymond <ethan.raymond14@gmail.com>
2016-05-19 20:15:54 +0800

    general info server side method

--- common/models/general-info.js
+++ common/models/general-info.js
@@ -1,3 +1,8 @@
 module.exports = function(GeneralInfo) {
-
+        GeneralInfo.getUpdate = function(date, cb) {
+        _d = new Date(date);
+        GeneralInfo.find({where: {lastModified: {gt: _d}}},function (err, posts) {
+            cb(null, posts);
+            console.log("err: " + err);});
+    }
 };
--- common/models/note.js
+++ common/models/note.js
@@ -2,12 +2,6 @@ module.exports = function(Poi) {
         Poi.getUpdate = function(date, cb) {
         _d = new Date(date);
         Poi.find({where: {lastModified: {gt: _d}}},function (err, posts) {
-            if (null != posts && posts.length > 0) {
-                response = "Name of coffee shop is " + posts[0].name;
-            }
-            else {
-                response = "no posts";
-            }
             cb(null, posts);
             console.log("err: " + err);});
     }
