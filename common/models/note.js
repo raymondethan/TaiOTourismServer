f2e8e01d277faf9e1ab86052899e8586963127fa
Ethan Raymond <ethan.raymond14@gmail.com>
2016-05-19 19:14:55 +0800

    fixed that stupid resource loading error with a hack

--- common/models/note.js
+++ common/models/note.js
@@ -1,8 +1,14 @@
-module.exports = function(Note) {
-  Note.greet = function(msg, cb) {
-    process.nextTick(function() {
-      msg = msg || 'world';
-      cb(null, 'Hello ' + msg);
-    });
-  };
+module.exports = function(Poi) {
+        Poi.getUpdate = function(date, cb) {
+        _d = new Date(date);
+        Poi.find({where: {lastModified: {gt: _d}}},function (err, posts) {
+            if (null != posts && posts.length > 0) {
+                response = "Name of coffee shop is " + posts[0].name;
+            }
+            else {
+                response = "no posts";
+            }
+            cb(null, posts);
+            console.log("err: " + err);});
+    }
 };
